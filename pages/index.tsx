9aff80bbc1627ec04b69e4fe4cfde94ec7a4a749
ito0627 <itoen320@gmail.com>
2025-01-19 19:48:30 +0900

    実機確認時レイアウト不具合修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const Home: React.FC = () => {
   return (
     <div className="bg-gray-200 min-h-screen">
       <div className="flex flex-col items-center p-4 w-11/12 h-screen mx-auto content-color rounded shadow-md">
-        <h1 className="text-6xl mb-3 tracking-widest font-mono">My Tasks</h1>
+        <h1 className="text-5xl mb-3 tracking-widest font-mono">My Tasks</h1>
         <TaskForm />
         <h2 className="text-4xl text-green-custom mt-20 mb-3 tracking-widest">
           LIST
