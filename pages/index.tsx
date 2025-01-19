65514a258c0b1e4796c588a346637d28c9abe21c
ito0627 <itoen320@gmail.com>
2025-01-19 20:04:10 +0900

    実機確認用コミット

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,8 @@ import TaskList from "../src/recoil/components/TaskList";
 
 const Home: React.FC = () => {
   return (
-    <div className="bg-gray-200 min-h-screen">
-      <div className="flex flex-col items-center p-4 w-11/12 h-screen mx-auto content-color rounded shadow-md">
+    <div className="bg-gray-200 min-h-screen flex items-center justify-center">
+      <div className="flex flex-col items-center p-4 w-11/12 h-5/6 mx-auto content-color rounded shadow-md">
         <h1 className="text-4xl mb-1 tracking-widest font-mono">My Tasks</h1>
         <TaskForm />
         <div className="task-list-container w-full max-w-lg mx-auto p-4">
